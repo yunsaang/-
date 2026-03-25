94bad9d3b7f5a0fc2d54f1e784213086abb09b18
yunsaang <atcusr1@gmail.com>
2026-03-25 16:03:57 +0900

    visual st 20260325
    f

---
+++ main.cpp
@@ -0,0 +1,440 @@
+#include <stdio.h>
+int main()
+{
+	for (int i = 1; i < 5; ++1)
+	{
+		printf("%d\n", i+1);
+	}
+}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n, 1");
+//	printf("%d\n, 2");
+//	printf("%d\n, 3");
+//	printf("%d\n, 4");
+//	printf("%d\n, 5");
+//}
+// 
+//#include <stdio.h>
+//int main()
+//{
+//	printf(%d\n,1);
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	int run = 1;
+//	while (run)
+//	{
+//		if (_getch() == 'q')
+//		{
+//			run = 0;
+//		}
+//		printf("hello\n");
+//	}
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	int run = 1;
+//	while ( run )
+//	{
+//		if (_getch() == 'q')
+//			run = 0;
+//		printf("hello\n");
+//	}
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	//반복문(for, while)
+//	while ( 1 )
+//	{
+//		if (_getch() == 'q')
+//			break;
+//		printf("hello\n");
+//		
+//	}
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (int i = 0; i < 5 ; i +=2 )
+//	{
+//		printf("hello : %d\n", i);
+//	}
+//	printf("\n");
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	int i = 0;
+//	for ( ;; )
+//	{
+//		printf("hello : %d\n", i);
+//	}
+//	printf("\n");
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	for (int i = 1; i <= 5; i++)
+//	{
+//		printf("hello : %d\n", i);
+//	}
+//	printf("\n");
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	for (int i = 1; i <= 5; i += 1)
+//	{
+//		printf("hello : %d\n", i);
+//	}
+//	printf("\n");
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	for (int i = 1; i <= 5; i = i + 1)
+//	{
+//		printf("hello : %d\n", i);
+//	}
+//	printf("\n");
+//	for (int i = 0; i <= 5; i = i + 1)
+//	{
+//		printf("hello : %d\n", i);
+//	}
+//	printf("\n");
+//	for (int i = 1; i <= 5; i = i + 1)
+//	{
+//		printf("hello : %d\n", i);
+//	}
+//	printf("\n");
+//}
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	int i = 1;
+//	for ( ; i <= 5; i = i + 1)
+//	{
+//		printf("hello : %d\n, 1");
+//	}
+//	printf("i => %d\n , i")
+//}
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	for (int i = 1; i <= 5; i = i + 1)
+//	{
+//		printf("hello : %d\n, 1");
+//	}
+//	printf("i => %d\n , i")
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	for (int i = 1; i <= 5; i = i + 1)
+//	{
+//		printf("hello : %d\n, 1");
+//	}
+//}
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	// 반복문(for , while)
+//	// 1. 초기화
+//    // 2. 조건
+//	// 3. 증감
+//	for (int i = 1; i <= 100; i = i + 1)
+//	{
+//		printf("hello\n");
+//	}
+//}
+
+
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	// 반복문(for , while)
+//	printf("hello");
+//	printf("hello");
+//	printf("hello");
+//	printf("hello");
+//	printf("hello");
+//	printf("hello");
+//}
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	switch (2)
+//	{
+//		switch (_getch())
+//
+//	case '1':
+//		printf("111\n");
+//		break;
+//	case '2': 
+//		printf("222\n");
+//	case 3:
+//		printf("333\n");
+//		break;
+//	default:
+//		printf("default\n");
+//		break;
+//
+//
+//	}
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	switch (2)
+//	{
+//		switch ( _getch() )
+//		 
+//	case 49: // '1'
+//		printf("111\n");
+//		break;
+//	case '2': // 50
+//		printf("222\n");
+//	case 3:
+//		printf("333\n");
+//		break;
+//	default:
+//		printf("default\n");
+//		break;
+//
+//
+//	}
+
+
+//#include <stdio.h>
+//#include <conio.h>
+//int main()
+//{
+//	int c = _getch();
+//	printf("char : %c\n, c");
+//		printf("char : %d\n, c");
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	switch (2)
+//	{
+//	case 1:
+//		printf("111\n");
+//		break;
+//	case 2:
+//		printf("222\n");
+//	case 3:
+//		printf("333\n");
+//		break;
+//	default:
+//		printf("default\n");
+//		break;
+//
+//
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	switch (10)
+//	{
+//	case 1:
+//		printf("111\n");
+//		break;
+//	case 2:
+//		printf("222\n");
+//		break;
+//	case 3:
+//		printf("333\n");
+//		break;
+//	default:
+//		printf("default\n");
+//		break;
+//
+//
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	switch (2)
+//	{
+//	case 1:
+//		printf("111\n");
+//		break;
+//	case 2:
+//		printf("222\n");
+//		break;
+//	case 3:
+//		printf("333\n");
+//		break;
+//	default:
+//	    printf("default\n");
+//		break;
+//
+//
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	if (a == b)
+//	{
+//		printf("a == b\n");
+//	}
+//	else
+//	{
+//		printf("hello\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	if (a <= b)
+//	{
+//		printf("a <= b\n");
+//	}
+//	else
+//	{
+//		printf("hello\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	if (a <= b)
+//	{
+//		printf("a <= b\n");
+//
+//	}
+//	if (a > b)
+//	{
+//		printf("hello\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	if (a <= b)
+//	{
+//		printf("a <= b\n");
+//		
+//	}
+//	printf("hello\n");
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	if (a == 10)
+//	{
+//		printf("a == 10\n");
+//		printf("a == 10\n");
+//		printf("a == 10\n");
+//	}
+//	printf("hello\n");
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	if (a == 10)
+//	{
+//		printf("a == 10\n");
+//	}
+//	
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10, b = 20, result;
+//	
+//
+//	result = a + b;
+//	printf("%d , %d , %d\n", a, b, result);
+//}
+
+
+//#include <studio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int result;
+//
+//	result = a + b;
+//	print("%d , %d , %d\n", a, b, result);
+//}
+
+//#include <studio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	print(" % d, % d\n", a, b);
+//	print("%d\n", a + b);
+//	a = 20
+//	print("%d\n", a + b);
+//	b= 50
+//	print("%d\n", a + b);
+//}
