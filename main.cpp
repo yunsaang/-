827bbf2ff4c032b4adaab1daccd462da45302e09
yunsaang <atcusr1@gmail.com>
2026-06-10 16:30:06 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,400 +1,314 @@
-//# include <stdio.h>
+//#include <stdio.h>
 //int main()
 //{
-//	const char* s1 = "Hello!";
-//	char s2[7] = "Hello!";
-//
-//	printf("%c $c\n", s1[0], s2[1]);
-//
+//	return 0; //종료 코드(0)
 //}
 
-//# include <stdio.h>
+//#include <stdio.h>
+//void* Readfile()
+//{
+//	return NULL;
+//}
 //int main()
 //{
-//	const char* s1 = "Hello!";
-//	char s2[7] = "Hello!";
+//	if(NULL == ReadFile()
+//		return -1;
 //
-//	printf("%c %c\n", s1[0], s2[1]);
-//	printf("%s %s\n", s1, s2);
-//	printf("%s %s\n", &s1[0]);
-//	printf("%s %s\n", &s2[0]);
+//		//정상 처리 
+//
+//	return 0; //종료 코드(0)
 //}
 
-//# include <stdio.h>
+//#include <stdio.h>
+//void print(int a, int b)
+//{
+//	printf("%d , %d\n", a, b);
+//}
 //int main()
 //{
-//	const char* s1 = "Hello!";
-//	char s2[7] = "Hello!";
+//	int a = 10;
+//	int b = 20;
 //
-//	printf("%s\n", s1);
-//	printf("%s\n", s2);
+//	print(a, b);
+//	//
+//	a = b;
+//	b = a;
+//	print(a, b);
 //}
 
-//# include <stdio.h>
-//void PrintString(const char* s1)
+//#include <stdio.h>
+//void print(int a, int b)
 //{
-//	printf("%s\n", s1);
+//	printf("%d , %d\n", a, b);
+//}
+//void Swap(int* pa, int* pb)
+//{
+//	int t = *pa;
+//	*pa = *pb;
+//	*pb = t;
 //}
 //int main()
 //{
-//	const char* s1 = "Hello!";
-//	char s2[7] = "Hello!";
+//	int a = 10;
+//	int b = 20;
 //
-//	//printf("%s\n", s1);
-//	PrintString(s1);
-//	printf("%s\n", s2);
+//	print(a, b);
+//	Swap(&a, &b);
+//	print(a, b);
 //}
 
-//# include <stdio.h>
-//void PrintString(const char* s1)
+//#include <stdio.h>
+//void Print(int a, int b)
 //{
-//	printf("%s\n", s1);
+//	printf("%d , %d\n", a, b);
+//}
+//void Swap(int* pa, int* pb)
+//{
+//	int t = *pa;
+//	*pa = *pb;
+//	*pb = t;
 //}
 //int main()
 //{
-//	const char* s1 = "Hello!";
-//	char s2[7] = "Hello!";
+//	int a = 10;
+//	int b = 20;
 //
-//	//printf("%s\n", s1);
-//	PrintString(s1);
-//	//printf("%s\n", s2);
-//	PrintString(s2);
+//	Print(a, b);
+//	Swap(&a, &b);
+//	Print(a, b);
 //}
 
-//# include <stdio.h>
-//void PrintString(const char* s1)
+//#include <stdio.h>
+//void Print(int a, int b)
 //{
-//	printf("%s\n", s1);
-//  
+//	printf("%d , %d\n", a, b);
+//}
+//void Swap(int* pa, int* pb)
+//{
+//	int t = *pa;
+//	*pa = *pb;
+//	*pb = t;
 //}
 //int main()
 //{
-//	const char* s1 = "Hello!";
-//	char s2[7] = "Hello!";
-//
-//	//printf("%s\n", s1);
-//	PrintString(s1);
-//	//printf("%s\n", s2);
-//	PrintString(s2);
-//}
-
-//# include <stdio.h>
-//int main()
-//{
-//	int a[4];
-//	a[0] = 1;
-//	a[1] = 2;
-//	a[2] = 3;
-//	a[3] = 4;
-//}
-
-//# include <stdio.h>
-//int main()
-//{
-//	int a[4];
-//	
-//	for(int i = 0; i<4; ++i)
-//	    a[i] = i + 1;
-//
-//	printf("%5d", a[0]);
-//	printf("%5d", a[1]);
-//	printf("%5d", a[2]);
-//	printf("%5d", a[3]);
-//	printf("\n");
-//
-//}
-
-//# include <stdio.h>
-//int main()
-//{
-//	int a[4];
-//
-//	for (int i = 0; i < 4; ++i)
-//		a[i] = i + 1;
-//
-//	for (int i = 0; i < 4; ++i)
-//	printf("%5d", a[i]);
-//	printf("\n");
-//
-//}
-
-//# include <stdio.h>
-//# include <stdlib.h>
-//int main()
-//{
-//	int a[4];
-//
-//	for (int i = 0; i < 4; ++i)
-//		a[i] = i + 1;
-//
-//	for (int i = 0; i < 4; ++i)
-//		printf("%5d", a[i]);
-//	printf("\n");
-//	//////
-//	int* b = NULL;
-//	b = (int *)malloc(sizeof(int) * 4);
-//	b[0] = 1;
-//	b[1] = 2;
-//	b[2] = 3;
-//	b[3] = 4;
-//}
-
-//# include <stdio.h>
-//# include <stdlib.h>
-//int main()
-//{
-//	int a[4];
-//
-//	for (int i = 0; i < 4; ++i)
-//		a[i] = i + 1;
-//
-//	for (int i = 0; i < 4; ++i)
-//		printf("%5d", a[i]);
-//	printf("\n");
-//	//////
-//	int* b = NULL;
-//	b = (int*)malloc(sizeof(int) * 4);
-//
-//	for (int i = 0; i < 4; ++i)
-//		b[i] = i + 1;
-//
-//	for (int i = 0; i < 4; ++i)
-//		printf("%5d", b[i]);
-//	printf("\n");
-//
-//	free(b);
-//}
-
-//# include <stdio.h>
-//# include <stdlib.h>
-//void PrintData(int data)
-//{
-//	printf("%d\n", data);
-//	return;
-//}
-//int main()
-//{  
-//	//함수
 //	int n = 10;
+//	int arr[3] = { 1,2,3 };
 //
-//	PrintData(n);
+//	printf("%p\n", &n);
+//	printf("%p\n", arr);
+//	printf("%p\n", Print);
+//	printf("%p\n", Swap);
 //}
 
-//# include <stdio.h>
-//# include <stdlib.h>
-//void PrintData(int data)
+//#include <stdio.h>
+//void Print(int a, int b)
 //{
-//	// data = 600;
-//	printf("%d\n", data);
-//	return;
+//	printf("%d , %d\n", a, b);
 //}
-//void PrintDataRef(const int* pdata) // 중요
+//void Swap(int* pa, int* pb)
 //{
-//	*pdata = 600;
-//	printf("%d\n", *pdata);
-//	return;
+//	int t = *pa;
+//	*pa = *pb;
+//	*pb = t;
 //}
 //int main()
 //{
-//	//함수
 //	int n = 10;
+//	int* p1 = &n;
 //
-//	PrintData(n);
-//	PrintDataRef(&n);
+//	void (*p2)(int, int);
+//	p2 = Print;
+//
+//	int arr[3] = { 1,2,3 };
+//
+//	printf("%p\n", &n);
+//	printf("%p\n", arr);
+//	printf("%p\n", Print);
+//	printf("%p\n", Swap);
 //}
 
-//# include <stdio.h>
-//# include <stdlib.h>
-//void PrintData(int data)
+//#include <stdio.h>
+//void Print(int a, int b)
 //{
-//	// data = 600;
-//	printf("%d\n", data);
-//	return;
+//	printf("%d , %d\n", a, b);
 //}
-//void PrintDataRef(const int* pdata) 
+//void Swap(int* pa, int* pb)
 //{
-//	printf("%d\n", pdata[0]);
-//	printf("%d\n", pdata[1]);
-//	return;
+//	int t = *pa;
+//	*pa = *pb;
+//	*pb = t;
 //}
 //int main()
 //{
-//	//함수
-//	int arr[2] = { 10, 20 };
-//	
-//
-//	// PrintData(arr[2]); //value
-//	PrintData(arr); //reference
-//}
-
-//**
-//# include <stdio.h>
-//# include <stdlib.h>
-//void PrintData(int data)
-//{
-//	printf("%d\n", data);
-//	return;
-//}
-//void PrintDataRef(const int* pdata)
-//{
-//	printf("%d\n", *pdata);
-//	
-//}
-//void SetData(int* pdata, int value)
-//{
-//	*pdata = value;
-//}
-//int main()
-//{
-//	int data = 10;
-//	
-//	//SetData(data, 50); x
-//	SetData(&data, 50);
-//
-//	PrintData(data);
-//	PrintDataRef(&data);
-//}
-
-//# include <stdio.h>
-//void PrintArray(const int* arr)
-//{
-//	printf("%d\n", arr[0]);
-//	printf("%d\n", arr[1]);
-//	printf("\n");
-//}
-//int main()
-//{
-//	int arr[2] = { 10, 20 };
-//
-//	PrintArray(arr);
-//}
-
-//# include <stdio.h>
-//void PrintArray(const int* arr)
-//{
-//	printf("%d\n", arr[0]);
-//	printf("%d\n", arr[1]);
-//	printf("\n");
-//}
-//void IncrementArray(int* arr)
-//{
-//	++arr[0];
-//	++arr[1];
-//}
-//int main()
-//{
-//	int arr[2] = { 10, 20 };
-//
-//	IncrementArray(arr);
-//	PrintArray(arr);
-//}
-
-//# include <stdio.h>
-//int main()
-//{
-//	// 입력 함수 _CRT_SECURE_NO_WARNINGS
 //	int n = 10;
+//	int* p1 = &n;
 //
-//	scanf("%d", &n);
-//	printf("%d\n", n);
-//}
-
-//# include <stdio.h>
-//int main()
-//{
-//	// 입력 함수 _CRT_SECURE_NO_WARNINGS
-//	int n = 10;
+//	void (*p2)(int, int);
+//	p2 = Print;
 //
-//	printf("input integer :");
-//	scanf("%d", &n);
+//	int arr[3] = { 1,2,3 };
 //
-//	printf("data : %d\n", n);
-//}
-
-//# include <stdio.h>
-//int main()
-//{
-//	// 입력 함수 _CRT_SECURE_NO_WARNINGS
-//	int a = 0, b =0;
-//
-//	printf("input integer :");
-//	scanf("%d %d ", &a, &b);
-//
-//	printf("data : %d %d\n", a, b);
-//}
-
-//# include <stdio.h>
-//int main()
-//{
-//	double a = 0, b = 0;
-//
-//	printf("input integer :");
-//	scanf("%lf %lf", &a, &b);
-//
-//	printf("data : %g %g\n", a, b);
-//}
-
-//# include <stdio.h>
-//# include <stdlib.h>
-//int main()
-//{
-//	int* arr = NULL;
-//	int capacity = 0;
-//
-//	printf("input capacity : ");
-//	scanf("%d", &capacity);
-//
-//	arr = (int*)malloc(sizeof(int) * capacity);
-//
-//	for(int i = 0; i < capacity; ++i)
-//		arr[i] = i + 1;
-//
-//	for (int i = 0; i < capacity; ++i)
-//		printf("%d", arr[i]);
-//	printf("\n");
+//	printf("%p\n", *p1);
+//	printf(1,2);
 //	
-//	free(arr);
 //}
 
-//# include <stdio.h>
-//# include <stdlib.h>
+//#include <stdio.h>
+//void Print(int a, int b)
+//{
+//	printf("%d , %d\n", a, b);
+//}
+//void Swap(int* pa, int* pb)
+//{
+//	int t = *pa;
+//	*pa = *pb;
+//	*pb = t;
+//}
 //int main()
 //{
-//	int* arr = NULL;
-//	int capacity = 0;
+//	int a = 10, b = 20;
 //
-//	printf("input capacity : ");
-//	scanf("%d", &capacity);
+//	Print(a, b);
+//	Swap(&a, &b);
+//	Print(a, b);
 //
-//	arr = (int*)malloc(sizeof(int) * capacity);
+//	void (*pPrint)(int a, int b) = Print;
+//	void (*pSwap)(int* pa, int* pb) = Swap;
 //
-//	for (int i = 0; i < capacity; ++i)
-//		arr[i] = i + 1;
-//
-//	for (int i = 0; i < capacity; ++i)
-//		printf("%d", arr[i]);
-//	printf("\n");
-//
-//	free(arr);
+//	pPrint(a, b);
+//	pSwap(&a, &b);
+//	pPrint(a, b);
 //}
 
-# include <stdio.h>
-# include <stdlib.h>
+//#include <stdio.h>
+//void PrintData(const int* pa, int size)
+//{
+//	for (int i = 0; i < size; ++i)
+//		printf("[%d]", pa[i]);
+//	printf("\n");
+//}
+////client
+//int main()
+//{
+//	int a[10] = { 10, 50, 80, 30, 45, 62, 53, 42, 43, 70 };
+//	PrintData(a, 10);
+//}
+
+//#include <stdio.h>
+//void PrintData(const int* pa, int size, void (*pf)(int ))
+//{
+//	for (int i = 0; i < size; ++i)
+//		pf(pa[i]);
+//	printf("\n");
+//}
+////client
+//void Print1(int data)
+//{
+//	if (data % 2 == 0)
+//		printf("[%d]", data);
+//}
+//int main()
+//{
+//	int a[10] = { 10, 50, 80, 30, 45, 62, 53, 42, 43, 70 };
+//	PrintData(a, 10, Print1);
+//}
+
+//#include <stdio.h>
+////server
+//void PrintData(const int* pa, int size, void (*pf)(int))
+//{
+//	for (int i = 0; i < size; ++i)
+//		pf(pa[i]); //server -> client (callback)
+//	printf("\n");
+//}
+////client
+//void Print1(int data) //callback function
+//{
+//	if (data % 2 == 0)
+//		printf("[%d]", data);
+//}
+//void Print2(int data)
+//{
+//	printf("[%d]", data);
+//}
+//void Print3(int data)
+//{
+//	printf("==");
+//	printf("[%d]", data);
+//	printf("==\n");
+//}
+//int main()
+//{
+//	int a[10] = { 10, 50, 80, 30, 45, 62, 53, 42, 43, 70 };
+//	PrintData(a, 10, Print1); //client -> server(call)
+//	PrintData(a, 10, Print2);
+//	PrintData(a, 10, Print3);
+//}
+
+//#include <stdio.h>
+////server
+//int FindData(int* pa, int size, int key)
+//{
+//	for (int i = 0; i < size; ++i)
+//		if (pa[i] == key)
+//			return i;
+//	return -1;
+//}
+////client
+//int main()
+//{
+//	int arr[10] = { 5,64,98,25,35,74,69,88,85,27 };
+//	int index = FindData(arr, 10, 74);
+//
+//	if (index != -1)
+//	{
+//		printf("[%d] : %d\n", index, arr[index]);
+//	}
+//}
+
+//#include <stdio.h>
+////server
+//int FindData(int* pa, int size, int key)
+//{
+//	for (int i = 0; i < size; ++i)
+//		if (pa[i] == key)
+//			return i;
+//	return -1;
+//}
+////client
+//int main()
+//{
+//	int arr[10] = { 5,64,98,25,35,74,69,88,85,27 };
+//	int index = FindData(arr, 10, 74);
+//
+//	if (index != -1)
+//	{
+//		printf("[%d] : %d\n", index, arr[index]);
+//	}
+//}
+
+#include <stdio.h>
+//server
+int FindData(int* pa, int size, int (*cmp(int))
+{
+	for (int i = 0; i < size; ++i)
+		if (cmp(pa[i]))
+			return i;
+
+	return -1;
+}
+//client
+int Predicate1(int key)
+{
+	return key > 50 && key % 5 == 0;
+}
 int main()
 {
-	int* arr = NULL;
-	int capacity = 0;
+	int arr[10] = { 5,64,98,25,35,74,69,88,85,27 };
+	int index = FindData(arr, 10, 74);
 
-	printf("input capacity : ");
-	scanf("%d", &capacity);
-
-	// int arr[capacity]; 안됨
-	for (int i = 0; i < capacity; ++i)
-		arr[i] = i + 1;
-
-	for (int i = 0; i < capacity; ++i)
-		printf("%d", arr[i]);
-	printf("\n");
-
-
+	if (index != -1)
+	{
+		printf("[%d] : %d\n", index, arr[index]);
+	}
 }
